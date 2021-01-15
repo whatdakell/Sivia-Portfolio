e6d31fb668847f3823538955e29c3496248e8c09
kelly-linehan <klinehan@apple.com>
2021-01-14 22:19:38 -0800

    SOOO messy

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -94,8 +94,64 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-// Write Code Here
 
+
+var controller = new ScrollMagic.Controller();
+
+console.log('hello', controller);
+
+$(".fast").each(function () {
+  var tl = new TimelineMax();
+  var child = $(this).find("img");
+  tl.to(child, 1, { y: -180, ease: Linear.easeNone });
+  var scene = new ScrollMagic.Scene({
+    triggerElement: $('.hero-images'),
+    triggerHook: 0.4,
+    duration: "100%"
+  }).setTween(tl)
+  // .addIndicators({
+  //   colorTrigger: "black",
+  //   colorStart: "black",
+  //   colorEnd: "black",
+  //   indent: 10
+  // })
+  .addTo(controller);
+});
+
+$(".faster").each(function () {
+  var tl = new TimelineMax();
+  var child = $(this).find("img");
+  tl.to(child, 1, { y: -180, ease: Linear.easeNone });
+  var scene = new ScrollMagic.Scene({
+    triggerElement: $('.hero-images'),
+    triggerHook: 0.4,
+    duration: "100%"
+  }).setTween(tl)
+  // .addIndicators({
+  //   colorTrigger: "black",
+  //   colorStart: "black",
+  //   colorEnd: "black",
+  //   indent: 10
+  // })
+  .addTo(controller);
+});
+$(".slow").each(function () {
+  var tl = new TimelineMax();
+  var child = $(this).find("img");
+  tl.to(child, 1, { y: -180, ease: Linear.easeNone });
+  var scene = new ScrollMagic.Scene({
+    triggerElement: $('.hero-images'),
+    triggerHook: 0.4,
+    duration: "100%"
+  }).setTween(tl)
+  // .addIndicators({
+  //   colorTrigger: "black",
+  //   colorStart: "black",
+  //   colorEnd: "black",
+  //   indent: 10
+  // })
+  .addTo(controller);
+});
 
 /***/ })
 
